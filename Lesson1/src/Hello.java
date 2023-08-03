628470a13b508a0b6aa87172a2d6db2d9c05dd71
Anahit Khachatryan <ankhachatryan1995@gmail.com>
2023-08-03 12:22:45 +0400

    My first git commit

---
+++ Lesson1/src/Hello.java
@@ -0,0 +1,47 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java 8");
+        System.out.print("Barev Anul\n");
+
+        byte x = 124;
+        System.out.println(x);
+
+        short y = 26526;
+        System.out.println(y);
+
+        int z = 62625626;
+        System.out.println(z);
+
+        long f = 261526555555555555L;
+        System.out.println(f);
+
+        // Kamayakan amboxj tiv java-um int e
+
+        float h = 45.3f;
+        double d = 455.638;
+
+        // Kamayakan racional tiv java-um double e
+
+        boolean t = true;
+        System.out.println(t);
+        t = false;
+        System.out.println(t);
+
+
+
+        char g = '\u003F';
+        System.out.println(g);
+
+        String u = "Hello java 8";
+        System.out.println(u);
+
+        String FirstName = "Anul";
+        String LastName = "Khachatryan";
+        System.out.println(FirstName + " " + LastName);
+
+        int l = 68;
+        System.out.println("l = " + l);
+        System.out.println("END");
+
+    }
+}
